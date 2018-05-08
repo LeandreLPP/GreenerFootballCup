89fb1ce1f8da92583bda409c9a781fb6502a5f2b
Enzo Menegaldo <enzo.menegaldo@insa-rennes.fr>
2018-05-08 19:43:24 +0200

    add year picker

--- app/src/main/java/ltu/course/mobile/project/greenerfootballcup/LoginActivity.java
+++ app/src/main/java/ltu/course/mobile/project/greenerfootballcup/LoginActivity.java
@@ -1,24 +1,31 @@
 package ltu.course.mobile.project.greenerfootballcup;
 
+import android.app.DatePickerDialog;
+import android.app.Dialog;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.text.method.KeyListener;
 import android.text.method.PasswordTransformationMethod;
 import android.view.KeyEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.NumberPicker;
 import android.widget.TextView;
 
 import java.text.DateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.Date;
 
 import ltu.course.mobile.project.greenerfootballcup.utilities.LoginDatas;
 
 public class LoginActivity extends AppCompatActivity {
 
+    private static final int minYear = 2000;
     private EditText admin_code ;
     private EditText confirm_admin_Code;
     private EditText admin_email;
@@ -46,22 +53,24 @@ public class LoginActivity extends AppCompatActivity {
 
             @Override
             public void onTextChanged(CharSequence s, int start, int before, int count) {
-                if(current_year.getText().length() != 4){
-                    LoginDatas.getInstance().setYear(null);
-                    current_year.setError("syntax error");
-                    if(btnToScreen2.isEnabled())
-                        btnToScreen2.setEnabled(false);
-                }else{
-                    LoginDatas.getInstance().setYear(new Date(Integer.parseInt(current_year.getText().toString()),0,0));
-                    if(LoginDatas.getInstance().isInitialized())
+                if(!current_year.getText().toString().equals("")) {
+                    LoginDatas.getInstance().setYear(new Date(Integer.parseInt(current_year.getText().toString()), 0, 0));
+                    if (LoginDatas.getInstance().isInitialized())
                         btnToScreen2.setEnabled(true);
                 }
-
             }
 
             @Override
             public void afterTextChanged(Editable s) {
 
+            }
+        });
+
+        current_year.setKeyListener(null);
+        current_year.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openYearPicker();
             }
         });
 
@@ -181,5 +190,39 @@ public class LoginActivity extends AppCompatActivity {
                 startActivity(myIntent);
             }
         });
+    }
+
+    public void openYearPicker() {
+
+        final Dialog dialogPicker = new Dialog(LoginActivity.this);
+        dialogPicker.setTitle(R.string.title_year_picker);
+        dialogPicker.setContentView(R.layout.number_picker);
+        Button confirm_year = (Button) dialogPicker.findViewById(R.id.confirm_year);
+        Button cancel_year = (Button) dialogPicker.findViewById(R.id.cancel_year);
+        NumberPicker np = (NumberPicker) dialogPicker.findViewById(R.id.numberPicker);
+
+        String[] years = getYears(minYear,Calendar.getInstance().get(Calendar.YEAR));
+        np.setDisplayedValues(years);
+        np.setMaxValue(years.length-1);
+
+        confirm_year.setOnClickListener(v -> {
+            current_year.setText(String.valueOf(years[np.getValue()]));
+            dialogPicker.dismiss();
+        });
+
+
+        cancel_year.setOnClickListener(v -> dialogPicker.dismiss());
+        dialogPicker.show();
+
+    }
+
+    public String[] getYears(int minimumInclusive, int maximumInclusive) {
+
+        ArrayList<String> result = new ArrayList<String>();
+
+        for(int i = maximumInclusive; i >= minimumInclusive; i--) {
+            result.add(Integer.toString(i));
+        }
+        return result.toArray(new String[0]);
     }
 }
