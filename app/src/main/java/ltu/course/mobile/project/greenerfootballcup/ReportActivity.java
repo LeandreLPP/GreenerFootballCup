eaddf2b4772a96631bf7041c315221cfffd10bb7
Léandre Le Polles--Potin <leandre.lepolles@free.Fr>
2018-05-27 17:20:22 +0200

    Debugged the taking and saving of pictures.

--- app/src/main/java/ltu/course/mobile/project/greenerfootballcup/ReportActivity.java
+++ app/src/main/java/ltu/course/mobile/project/greenerfootballcup/ReportActivity.java
@@ -6,8 +6,14 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Matrix;
+import android.media.ExifInterface;
 import android.net.Uri;
-import android.preference.PreferenceManager;
+import android.os.AsyncTask;
+import android.os.Debug;
+import android.os.Environment;
+import android.os.Handler;
 import android.provider.MediaStore;
 import android.support.annotation.NonNull;
 import android.support.annotation.StringRes;
@@ -18,6 +24,8 @@ import android.support.v4.content.FileProvider;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.util.Log;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -25,11 +33,18 @@ import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.PopupWindow;
+import android.widget.ProgressBar;
 import android.widget.Toast;
 
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.lang.reflect.Executable;
+import java.net.URI;
+import java.util.logging.Logger;
+
+import ltu.course.mobile.project.greenerfootballcup.utilities.ConfirmationDialogFragment;
+import ltu.course.mobile.project.greenerfootballcup.utilities.ParserHTML;
 
 public class ReportActivity extends AppCompatActivity {
 
@@ -42,57 +57,101 @@ public class ReportActivity extends AppCompatActivity {
     private static final int REQUEST_CAPTURE_FAIRPLAY = 1527;
     private PopupWindow popupWindow;
 
+    private File backupFile;
+
+    private boolean hasAutorisationCamera;
+    private ProgressBar progressBarResult, progressBarFairplay;
+
+    private Handler uiHandler;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_report);
 
+        hasAutorisationCamera = false;
+
         imageViewResult = findViewById(R.id.imageViewResult);
         imageViewFairplay = findViewById(R.id.imageViewFairplay);
         imageViewReport = findViewById(R.id.imageViewReport);
 
-        fileImgResult = new File(getFilesDir(), "pictureResults.png");
-        fileImgFairplay = new File(getFilesDir(), "pictureFairplay.png");
+        progressBarResult = findViewById(R.id.progressBarResult);
+        progressBarFairplay = findViewById(R.id.progressBarFairplay);
+
+        File dir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+
+        fileImgResult = new File(dir, "pictureResults.jpg");
+        fileImgFairplay = new File(dir, "pictureFairplay.jpg");
+        backupFile = new File(dir, "backup.jpg");
         fileReport = new File(getFilesDir(), "report.pdf");
 
-        fileImgResult.delete();
-        fileImgFairplay.delete();
-        imageViewResult.setOnClickListener(new ImageTakerListener(fileImgResult, REQUEST_CAPTURE_RESULT));
-        imageViewFairplay.setOnClickListener(new ImageTakerListener(fileImgFairplay, REQUEST_CAPTURE_FAIRPLAY));
-        //imageViewReport.setImageURI(Uri.fromFile(fileReport));
+        imageViewResult.setOnClickListener((c) -> {
+            if(fileImgResult.exists())
+                openPopupImage(fileImgResult, REQUEST_CAPTURE_RESULT);
+            else
+                takePicture(REQUEST_CAPTURE_RESULT);});
+        imageViewFairplay.setOnClickListener((c) -> {
+            if(fileImgResult.exists())
+                openPopupImage(fileImgFairplay, REQUEST_CAPTURE_FAIRPLAY);
+            else
+                takePicture(REQUEST_CAPTURE_FAIRPLAY);});
+
+        new LoadImage().execute(new Param(fileImgResult, imageViewResult, progressBarResult));
+        new LoadImage().execute(new Param(fileImgFairplay, imageViewFairplay, progressBarFairplay));
+        // TODO report image
 
         buttonSendReport = findViewById(R.id.buttonSendReport);
+        uiHandler = new Handler();
     }
 
     private static final String FRAGMENT_DIALOG = "dialog";
     @Override
     protected void onResume() {
         super.onResume();
+        checkAuthorisationCamera();
+    }
+
+    private void checkAuthorisationCamera(){
         if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA)
-            != PackageManager.PERMISSION_GRANTED) {
+            == PackageManager.PERMISSION_GRANTED)
+        {
+            hasAutorisationCamera = true;
+        }
+        else
+        {
+            hasAutorisationCamera = false;
             if (ActivityCompat.shouldShowRequestPermissionRationale(this,
-                                                                    Manifest.permission.CAMERA)) {
+                                                                    Manifest.permission.CAMERA))
+            {
                 ConfirmationDialogFragment
                         .newInstance(R.string.camera_permission_confirmation,
                                      new String[]{Manifest.permission.CAMERA},
                                      REQUEST_CAMERA_PERMISSION,
                                      R.string.camera_permission_not_granted)
                         .show(getSupportFragmentManager(), FRAGMENT_DIALOG);
-            } else {
+            }
+            else
+            {
                 ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA},
                                                   REQUEST_CAMERA_PERMISSION);
             }
         }
     }
+
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
                                            @NonNull int[] grantResults) {
         switch (requestCode) {
             case REQUEST_CAMERA_PERMISSION:
+                hasAutorisationCamera = false;
                 if (permissions.length != 1 || grantResults.length != 1) {
                     throw new RuntimeException("Error on requesting camera permission.");
                 }
-                if (grantResults[0] != PackageManager.PERMISSION_GRANTED) {
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED)
+                {
+                    hasAutorisationCamera = true;
+                }
+                else {
                     Toast.makeText(this, R.string.camera_permission_not_granted,
                                    Toast.LENGTH_SHORT).show();
                 }
@@ -101,119 +160,146 @@ public class ReportActivity extends AppCompatActivity {
         }
     }
 
-    public static class ConfirmationDialogFragment extends DialogFragment {
-
-        private static final String ARG_MESSAGE = "message";
-        private static final String ARG_PERMISSIONS = "permissions";
-        private static final String ARG_REQUEST_CODE = "request_code";
-        private static final String ARG_NOT_GRANTED_MESSAGE = "not_granted_message";
-
-        public static ConfirmationDialogFragment newInstance(@StringRes int message,
-                                                             String[] permissions, int requestCode, @StringRes int notGrantedMessage) {
-            ConfirmationDialogFragment fragment = new ConfirmationDialogFragment();
-            Bundle args = new Bundle();
-            args.putInt(ARG_MESSAGE, message);
-            args.putStringArray(ARG_PERMISSIONS, permissions);
-            args.putInt(ARG_REQUEST_CODE, requestCode);
-            args.putInt(ARG_NOT_GRANTED_MESSAGE, notGrantedMessage);
-            fragment.setArguments(args);
-            return fragment;
-        }
-
-        @NonNull
-        @Override
-        public Dialog onCreateDialog(Bundle savedInstanceState) {
-            final Bundle args = getArguments();
-            return new AlertDialog.Builder(getActivity())
-                    .setMessage(args.getInt(ARG_MESSAGE))
-                    .setPositiveButton(android.R.string.ok,
-                                       new DialogInterface.OnClickListener() {
-                                           @Override
-                                           public void onClick(DialogInterface dialog, int which) {
-                                               String[] permissions = args.getStringArray(ARG_PERMISSIONS);
-                                               if (permissions == null) {
-                                                   throw new IllegalArgumentException();
-                                               }
-                                               ActivityCompat.requestPermissions(getActivity(),
-                                                                                 permissions, args.getInt(ARG_REQUEST_CODE));
-                                           }
-                                       })
-                    /*.setNegativeButton(android.R.string.cancel,
-                                       new DialogInterface.OnClickListener() {
-                                           @Override
-                                           public void onClick(DialogInterface dialog, int which) {
-                                               Toast.makeText(getActivity(),
-                                                              args.getInt(ARG_NOT_GRANTED_MESSAGE),
-                                                              Toast.LENGTH_SHORT).show();
-                                           }
-                                       })*/
-                    .create();
-        }
-
-    }
-
-    private class ImageTakerListener implements View.OnClickListener {
-
-        private File file;
-        private int intentID;
-
-        public ImageTakerListener(File pictureFile, int intentID) {
-            file = pictureFile;
-            this.intentID = intentID;
-        }
-
-        @Override
-        public void onClick(View v) {
-            if (file.exists())
-            {
-                openPopupImage(file, intentID);
-            }
-            else
-            {
-                takePicture(file, intentID);
-            }
-        }
-    }
-
     private void openPopupImage(File imgFile, int intentID) {
         LayoutInflater layoutInflater = getLayoutInflater();
         View popupView = layoutInflater.inflate(R.layout.popup_camera, null);
+        popupView.setOnClickListener((c) -> popupWindow.dismiss());
         popupWindow = new PopupWindow(popupView, WindowManager.LayoutParams.WRAP_CONTENT,
                                       WindowManager.LayoutParams.WRAP_CONTENT);
         popupWindow.setFocusable(true);
 
         Button retake_picture = popupView.findViewById(R.id.buttonRetakePicture);
         ImageView imageView = popupView.findViewById(R.id.imageView);
+        ProgressBar progressBar = popupView.findViewById(R.id.progressBar);
 
-        imageView.setImageURI(Uri.fromFile(imgFile));
+        new LoadImage().execute(new Param(imgFile, imageView, progressBar));
 
-        retake_picture.setOnClickListener(v -> takePicture(imgFile, intentID));
+        retake_picture.setOnClickListener(v -> takePicture(intentID));
 
         popupWindow.showAtLocation(this.imageViewFairplay, Gravity.CENTER, 0, 0);
     }
 
-    private void takePicture(File photoFile, int intentID) {
+    private void takePicture(int intentID) {
+        checkAuthorisationCamera();
+        if(!hasAutorisationCamera){
+            Toast.makeText(this, R.string.camera_permission_not_granted, Toast.LENGTH_LONG).show();
+            return;
+        }
         Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
         if (takePictureIntent.resolveActivity(getPackageManager()) != null)
         {
-            if (!photoFile.exists())
+            try
             {
-                try
-                {
-                    photoFile.createNewFile();
-                }
-                catch (IOException e)
-                {
-                    Toast.makeText(this, "Could not create the picture file. Error: " + e,
-                                   Toast.LENGTH_LONG).show();
-                }
-            }
-            if(!photoFile.exists())
-                return;
+                File file = getPictureFile(intentID);
+                Uri photoURI = FileProvider.getUriForFile(this,
+                                                          "ltu.course.mobile.project.greenerfootballcup.fileprovider",
+                                                          file);
 
-            /*Uri photoURI = Uri.fromFile(photoFile);
-            takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);*/
-            startActivityForResult(takePictureIntent, intentID);
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                startActivityForResult(takePictureIntent, intentID);
+            }
+            catch (IOException e)
+            {
+                e.printStackTrace();
+                Toast.makeText(this, "Error while creating the temp file: "+e, Toast.LENGTH_LONG).show();
+            }
+        }
+    }
+
+    private File tempFile;
+    private File getPictureFile(int intentID) throws IOException {
+        File originalFile = intentID == REQUEST_CAPTURE_RESULT ? fileImgResult : fileImgFairplay;
+        String name = intentID == REQUEST_CAPTURE_RESULT ? "pictureResults" : "pictureFairplay";
+        if (originalFile.exists()){
+            originalFile.renameTo(backupFile);
+        }
+        File dir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        tempFile = File.createTempFile(name,".jpg", dir);
+
+        return tempFile;
+    }
+
+    private class LoadImage extends AsyncTask<Param, Void, Boolean> {
+
+        private Bitmap bmp;
+        private ImageView imgView;
+        private ProgressBar progressBar;
+
+        @Override
+        protected Boolean doInBackground(Param... params) {
+            progressBar = params[0].progressBar;
+            publishProgress();
+            File file = params[0].imageFile;
+            imgView = params[0].imgToLoad;
+            try
+            {
+                bmp = BitmapFactory.decodeFile(file.getAbsolutePath());
+                int nh = (int) ( bmp.getHeight() * (2048.0 / bmp.getWidth()) );
+                bmp = Bitmap.createScaledBitmap(bmp, 2048,  nh, true);
+
+                ExifInterface ei = new ExifInterface(file.getPath());
+                int orientation = ei.getAttributeInt(ExifInterface.TAG_ORIENTATION,
+                                                     ExifInterface.ORIENTATION_UNDEFINED);
+
+                Bitmap rotatedBitmap = null;
+                switch(orientation) {
+
+                    case ExifInterface.ORIENTATION_ROTATE_90:
+                        rotatedBitmap = rotateImage(bmp, 90);
+                        break;
+
+                    case ExifInterface.ORIENTATION_ROTATE_180:
+                        rotatedBitmap = rotateImage(bmp, 180);
+                        break;
+
+                    case ExifInterface.ORIENTATION_ROTATE_270:
+                        rotatedBitmap = rotateImage(bmp, 270);
+                        break;
+
+                    case ExifInterface.ORIENTATION_NORMAL:
+                    default:
+                        rotatedBitmap = bmp;
+                }
+                bmp = rotatedBitmap;
+                return bmp != null;
+            }
+            catch (Exception e)
+            {
+                Log.d("","Error while saving picture",e);
+                return false;
+            }
+        }
+
+        private Bitmap rotateImage(Bitmap source, float angle) {
+            Matrix matrix = new Matrix();
+            matrix.postRotate(angle);
+            return Bitmap.createBitmap(source, 0, 0, source.getWidth(), source.getHeight(),
+                                       matrix, true);
+        }
+
+        @Override
+        protected void onProgressUpdate(Void... values) {
+            super.onProgressUpdate(values);
+            progressBar.setVisibility(View.VISIBLE);
+        }
+
+        @Override
+        protected void onPostExecute(Boolean b) {
+            progressBar.setVisibility(View.INVISIBLE);
+            if(b)
+                imgView.setImageBitmap(bmp);
+        }
+    }
+
+    private class Param {
+        public File imageFile;
+        public ImageView imgToLoad;
+        public ProgressBar progressBar;
+        public Param(File f, ImageView v, ProgressBar b)
+        {
+            imageFile = f;
+            imgToLoad = v;
+            progressBar = b;
         }
     }
 
@@ -221,37 +307,21 @@ public class ReportActivity extends AppCompatActivity {
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         if(resultCode == RESULT_OK)
         {
-            File imageFile = null;
-            if(requestCode == REQUEST_CAPTURE_RESULT)
-                imageFile = fileImgResult;
-            else if(requestCode == REQUEST_CAPTURE_FAIRPLAY)
-                imageFile = fileImgFairplay;
+            File imgFile = requestCode == REQUEST_CAPTURE_RESULT ? fileImgResult : fileImgFairplay;
+            ImageView imgView = requestCode == REQUEST_CAPTURE_RESULT ? imageViewResult : imageViewFairplay;
+            ProgressBar progressBar = requestCode == REQUEST_CAPTURE_RESULT ? progressBarResult : progressBarFairplay;
 
-            Bitmap bitmap = (Bitmap) data.getExtras().get("data");
+            tempFile.renameTo(imgFile);
 
-            FileOutputStream out = null;
-            try {
-                out = new FileOutputStream(imageFile);
-                bitmap.compress(Bitmap.CompressFormat.PNG, 100, out); // bmp is your Bitmap instance
-                // PNG is a lossless format, the compression factor (100) is ignored
-            } catch (Exception e) {
-                e.printStackTrace();
-            } finally {
-                try {
-                    if (out != null) {
-                        out.close();
-                    }
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
+            new LoadImage().execute(new Param(imgFile, imgView, progressBar));
 
-            if(requestCode == REQUEST_CAPTURE_RESULT)
-                imageViewResult.setImageBitmap(bitmap);
-            else if(requestCode == REQUEST_CAPTURE_FAIRPLAY)
-                imageViewFairplay.setImageBitmap(bitmap);
+            if(popupWindow != null && popupWindow.isShowing())
+                popupWindow.dismiss();
 
             checkReportCompleteAndGenerate();
+        } else if (backupFile.exists()){
+            File imgFile = requestCode == REQUEST_CAPTURE_RESULT ? fileImgResult : fileImgFairplay;
+            backupFile.getAbsoluteFile().renameTo(imgFile);
         }
     }
 
